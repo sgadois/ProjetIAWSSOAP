a2c43aa87cccf3d3e65ac0e328020abb244f654c
Sebastien Gadois <sgadois@gmail.com>
2015-04-18 17:51:47 +0200

    Generate the response.

--- src/main/java/ugmontSOAP/ws/ListeSallesEndpoint.java
+++ src/main/java/ugmontSOAP/ws/ListeSallesEndpoint.java
@@ -1,9 +1,13 @@
 package ugmontSOAP.ws;
 
 import dao.ITheaterDao;
+import domain.Theater;
+import org.jdom2.Attribute;
+import org.jdom2.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.*;
-import org.w3c.dom.Element;
+
+import java.util.List;
 
 /**
  * Created by seb on 07/04/15.
@@ -20,21 +24,45 @@ public class ListeSallesEndpoint {
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "theatersRequest")
-    @Namespace(prefix = "rn", uri = NAMESPACE_URI) // nom de l'élément racine
+    @Namespace(prefix = "rn", uri = NAMESPACE_URI)
     @ResponsePayload
     public Element handleListeSallesRequest(@XPathParam("/rn:theatersRequest/rn:filmID/text()") String filmID)
             throws Exception {
 
-        //A faire : construire la reponse xml en allant chercher les salles dans la base de données
-        //List<Theater> t = dao.findByFilmId("tt0138902");
-        //System.out.println(t.get(0).getCity());
-        System.out.println("filmID : " + filmID);
 
+        //Récupère la liste des salles associées au filmID
+        List<Theater> t = dao.findByFilmId("tt0138902");
 
+        //On crée la racine XML;
+        org.jdom2.Namespace namespace = org.jdom2.Namespace.getNamespace("http://ugmontSOAP/theaters/schemas");
+        Element racine = new Element("theaters", namespace);
+        org.jdom2.Namespace XSI = org.jdom2.Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        racine.addNamespaceDeclaration(XSI);
+        racine.setAttribute("schemaLocation", "http://ugmontSOAP/theaters/schemas ListeSalles.xsd", XSI);
+        racine.setAttribute("filmID", filmID);
 
-        //Solution temporaire pour tester le code
-        Element elt = XmlHelper.getRootElementFromFileInClasspath("ListeSallesExemple.xml");
-        return elt;
+        for (Theater theater : t) {
+            Element e = new Element("theater", namespace);
+            e.setAttribute(new Attribute("id", theater.getId()));
+            racine.addContent(e);
 
+            Element city = new Element("city", namespace);
+            city.setText(theater.getCity());
+            e.addContent(city);
+
+            Element name = new Element("name", namespace);
+            name.setText(theater.getName());
+            e.addContent(name);
+
+            Element region = new Element("region", namespace);
+            region.setText(theater.getRegion());
+            e.addContent(region);
+
+            Element zipcode = new Element("zipcode", namespace);
+            zipcode.setText(theater.getZipcode());
+            e.addContent(zipcode);
+
+        }
+        return racine;
     }
 }
